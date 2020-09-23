eff3f8e91b73227c852021ee152a8d73933dbfd1
Ungrim <arkarash@gmail.com>
2020-09-23 14:41:48 +0200

    Angular Inicial Añadido

--- AngularStarted/ClientApp/src/app/app.module.ts
+++ AngularStarted/ClientApp/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleatComponent } from './empleat/empleat.component';
+import { EmpleatsComponent } from './empleats/empleats.component';
+import { EmpleatAddEditComponent } from './empleat-add-edit/empleat-add-edit.component';
+import {EmpleatService} from './services/empleat.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleatComponent,
+    EmpleatsComponent,
+    EmpleatAddEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    EmpleatService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ AngularStarted/ClientApp/src/app/empleats/empleats.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleats',
+  templateUrl: './empleats.component.html',
+  styleUrls: ['./empleats.component.scss']
+})
+export class EmpleatsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
